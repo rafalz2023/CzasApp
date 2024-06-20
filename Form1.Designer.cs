48f2fd86b78cdad877021bd452c1f9994fded66e
rafal <rafal@BIEDRA>
2024-06-20 20:37:32 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CzasApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label dateLabel;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.timeLabel.Location = new System.Drawing.Point(50, 50);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(158, 31);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Czas: 00:00";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.dateLabel.Location = new System.Drawing.Point(50, 100);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(178, 31);
+            this.dateLabel.TabIndex = 1;
+            this.dateLabel.Text = "Data: 01.01.2024";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "Form1";
+            this.Text = "Czas i Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CzasApp
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000; // 1 sekunda
+            timer.Tick += new EventHandler(UpdateDateTime);
+            timer.Start();
+        }
+
+        private void UpdateDateTime(object sender, EventArgs e)
+        {
+            timeLabel.Text = "Czas: " + DateTime.Now.ToString("HH:mm:ss");
+            dateLabel.Text = "Data: " + DateTime.Now.ToString("dd.MM.yyyy");
+        }
+    }
+}
